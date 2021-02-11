9afc6eacee6ba79b22071f9bdc426cdc82f88dfb
Sanish <sanishkr@gmail.com>
2021-02-11 18:05:25 +0530

    Update and rename firebase-message-sw.js to firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,15 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.6/firebase-messaging.js');
+
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('../firebase-messaging-sw.js')
+//   .then(function(registration) {
+//     console.log('Registration successful, scope is:', registration.scope);
+//   }).catch(function(err) {
+//     console.log('Service worker registration failed, error:', err);
+//   });
+// }
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
